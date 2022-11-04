170fc8b7b68cefa87d9527d476bb6df920f7d1b5
nkobaidze <nika.kobaidze@gmail.com>
2022-11-04 16:49:23 +0400

    [ADD] redirect

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,21 +1,31 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+
 import products from "../data";
 
 const SingleProduct = () => {
     console.log(useParams());
-
     console.log(products);
+
+    const navigate = useNavigate();
+    console.log(navigate);
+
     const { productId } = useParams();
 
     const product = products.find((product) => product.id === productId);
     console.log(product);
     const { image, name } = product;
 
+    const redirect = () => {
+        navigate("/");
+    };
     return (
         <section className="section product">
             <img src={image} alt={name} />
             <h5>{name}</h5>
             <h2>single product {productId}</h2>
+            <button onClick={redirect} className="btn">
+                GO HOME
+            </button>
         </section>
     );
 };
